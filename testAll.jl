938812794e470553ad98a66ba6f0ab9bf68077b7
St Elmo <stelmozors@gmail.com>
2015-02-18 13:49:18 +0200

    updated travis to run specific script

---
+++ testAll.jl
@@ -0,0 +1,10 @@
+# Testing function
+
+# Test the HMM
+println("Beginning HMM tests...")
+cd(".\\Hidden Markov Model")
+require("HMM_tests.jl")
+cd("..\\")
+println("Finished HMM tests")
+
+# Another test
