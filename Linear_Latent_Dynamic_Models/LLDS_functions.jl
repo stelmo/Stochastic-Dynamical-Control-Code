40624cde0a3ab1c1b255349f63b6781b46dccbf0
St Elmo <stelmozors@gmail.com>
2015-02-23 22:51:03 +0200

    started continuous models

---
+++ Linear_Latent_Dynamic_Models/LLDS_functions.jl
@@ -0,0 +1,5 @@
+module LLDS_functions
+
+
+
+end #module
---
+++ Linear_Latent_Dynamic_Models/LLDS_tests.jl
@@ -0,0 +1,12 @@
+## Linear Latent Dynamic Models
+# h - h -> ...
+# |   |
+# o   o
+# Tests for the Kalman type models.
+# All tests are compared to code supplied by:
+# Bayesian Reasoning and Machine Learning
+# by David Barber
+# Website: http://www0.cs.ucl.ac.uk/staff/d.barber/brml/
+# The example is taken from Example 24.4
+# The functions were all compared to his demo program in Matlab:
+# demoLDSTracking.m
