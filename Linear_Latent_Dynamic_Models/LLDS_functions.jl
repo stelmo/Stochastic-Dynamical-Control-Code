38a2788b005ef32d8f893a0edb255f0bfd3cf85f
St Elmo <stelmozors@gmail.com>
2015-02-25 12:39:42 +0200

    read in correct model  and plot

--- Linear_Latent_Dynamic_Models/LLDS_functions.jl
+++ Linear_Latent_Dynamic_Models/LLDS_functions.jl
@@ -2,7 +2,11 @@ module LLDS_functions
 
 using Distributions
 
-immutable LLDS{T} # so that univariate and multivariate systems can be handled easily
+immutable LLDS{T}
+  # T = Float64 or Array{Float64, 2} so that univariate
+  # and multivariate systems can be handled easily.
+  # NB: all matrices need to have two dimensions.
+  # Assume zero mean transition and emission functions.
   # The linear latent dynamical system should have the
   # state space form:
   # x(t+1) = A*x(t) + Bu(t+1) + Q
@@ -15,14 +19,49 @@ immutable LLDS{T} # so that univariate and multivariate systems can be handled e
   R :: T # Measurement Noise
 end
 
-function step(xprev::Float64, yprev::Float64, model::LLDS)
-
+function step(xprev::Float64, model::LLDS{Float64})
+  # No control, move univariate model one time step forward.
   dprocess = Normal(model.Q)
   dmeasure = Normal(model.R)
+  xnow = model.A*xprev + rand(dprocess)
+  ynow = model.C*xnow + rand(dmeasure)
 
   return xnow, ynow
 end
 
+function step(xprev::Float64, unow::Float64, model::LLDS{Float64})
+  # Controlled, move univariate model one time step forward.
+  dprocess = Normal(model.Q)
+  dmeasure = Normal(model.R)
+  xnow = model.A*xprev + model.B*unow + rand(dprocess)
+  ynow = model.C*xnow +model.D*unow + rand(dmeasure)
 
+  return xnow, ynow
+end
+
+function step(xprev::Array{Float64,1}, unow::Array{Float64,1}, model::LLDS{Array{Float64,2}})
+  # Controlled, move multivariate model one time step forward.
+  dprocess = MvNormal(model.Q)
+  dmeasure = MvNormal(model.R)
+  xnow = model.A*xprev + model.B*unow + rand(dprocess)
+  ynow = model.C*xnow +model.D*unow + rand(dmeasure)
+
+  return xnow, ynow
+end
+
+function step(xprev::Array{Float64,1}, model::LLDS{Array{Float64,2}})
+  # No control, move multivariate model one time step forward.
+  dprocess = MvNormal(model.Q)
+  dmeasure = MvNormal(model.R)
+  xnow = model.A*xprev + rand(dprocess)
+  ynow = model.C*xnow + rand(dmeasure)
+
+  return xnow, ynow
+end
+
+function filter()
+  # Step the filter by one time step
+  
+end
 
 end #module
--- Linear_Latent_Dynamic_Models/LLDS_tests.jl
+++ Linear_Latent_Dynamic_Models/LLDS_tests.jl
@@ -13,24 +13,50 @@
 # The functions were all compared to his demo program in Matlab:
 # demoLDSTracking.m
 
-import LLDS_functions
+using PyPlot
 
+import LLDS_functions
+reload("LLDS_functions.jl")
+
+# Specify System
 dt = 0.1
 T = 400
-A = eye(6)
+A = eye(6) # state transition
 A[1,5] = dt
 A[2,1] = dt
 A[3,6] = dt
 A[4,3] = dt
-B = zeros(6,1)
-C = zeros(2,6)
+B = zeros(6,1) # input transition
+C = zeros(2,6) # state observation
 C[1,2] = 1.0
 C[2,4] = 1.0
-D = zeros(2,1)
+D = zeros(2,1) # input observation
 
-Q = 1.0e-5*eye(6)
-Q[5,5] = 1.0e-2
-Q[6,6] = 1.0e-2
-R = 50.0*eye(2)
+sigmaQ = 0.00001 # standard deviation of process noise
+sigmaR = 50.0 # standard deviation of measurement noise
+Q = sigmaQ^2*eye(6) # process noise covariance
+R = sigmaR^2*eye(2) # measurement noise covariance
 
 model = LLDS_functions.LLDS{Array{Float64,2}}(A, B, C, D, Q, R)
+
+# Specify initial conditions
+init_covar = eye(6) # vague prior covar
+init_mean = zeros(6) # vague prior mean
+
+# Read in correct data
+hiddenstates = readcsv("hiddenstates.csv")
+visiblestates = readcsv("visiblestates.csv")
+
+filtercovar = readcsv("filtercovar.csv")
+filtermeans = readcsv("filtermeans.csv")
+
+smoothcovar = readcsv("smoothcovar.csv")
+smoothmeans = readcsv("smoothmeans.csv")
+
+
+# Plotting
+figure(1)
+plot(hiddenstates[2,1:10:end], hiddenstates[4,1:10:end], "ro")
+plot(visiblestates[1,:], visiblestates[2,:], "g.")
+plot(filtermeans[2, 1:10:end], filtermeans[4,1:10:end], "c>")
+plot(smoothmeans[2, 1:10:end], smoothmeans[4,1:10:end], "bs")
