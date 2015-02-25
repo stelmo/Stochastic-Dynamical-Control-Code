810a5a943b75079f684b0b8d6b444f1cdca3ca4a
St Elmo <stelmozors@gmail.com>
2015-02-25 11:06:42 +0200

    started implementing framework

--- Linear_Latent_Dynamic_Models/LLDS_functions.jl
+++ Linear_Latent_Dynamic_Models/LLDS_functions.jl
@@ -1,5 +1,28 @@
 module LLDS_functions
 
+using Distributions
+
+immutable LLDS{T} # so that univariate and multivariate systems can be handled easily
+  # The linear latent dynamical system should have the
+  # state space form:
+  # x(t+1) = A*x(t) + Bu(t+1) + Q
+  # y(t+1) = C*x(t+1) + Du(t+1) + R
+  A :: T
+  B :: T
+  C :: T
+  D :: T
+  Q :: T # Process Noise
+  R :: T # Measurement Noise
+end
+
+function step(xprev::Float64, yprev::Float64, model::LLDS)
+
+  dprocess = Normal(model.Q)
+  dmeasure = Normal(model.R)
+
+  return xnow, ynow
+end
+
 
 
 end #module
--- Linear_Latent_Dynamic_Models/LLDS_tests.jl
+++ Linear_Latent_Dynamic_Models/LLDS_tests.jl
@@ -1,4 +1,6 @@
 ## Linear Latent Dynamic Models
+# u   u
+# |   |
 # h - h -> ...
 # |   |
 # o   o
@@ -10,3 +12,25 @@
 # The example is taken from Example 24.4
 # The functions were all compared to his demo program in Matlab:
 # demoLDSTracking.m
+
+import LLDS_functions
+
+dt = 0.1
+T = 400
+A = eye(6)
+A[1,5] = dt
+A[2,1] = dt
+A[3,6] = dt
+A[4,3] = dt
+B = zeros(6,1)
+C = zeros(2,6)
+C[1,2] = 1.0
+C[2,4] = 1.0
+D = zeros(2,1)
+
+Q = 1.0e-5*eye(6)
+Q[5,5] = 1.0e-2
+Q[6,6] = 1.0e-2
+R = 50.0*eye(2)
+
+model = LLDS_functions.LLDS{Array{Float64,2}}(A, B, C, D, Q, R)
