f3b2a4d4511e02e2d38e7164fc6bd5ce7c700c75
St Elmo <stelmozors@gmail.com>
2015-03-09 12:49:37 +0200

    plot changes

--- Nonlinear_Latent_Dynamical_Models/Linear_Model.jl
+++ Nonlinear_Latent_Dynamical_Models/Linear_Model.jl
@@ -59,7 +59,7 @@ lin_cstr = begin
 end
 
 f(x, u, w) = A*x + B*u + b + w
-g(x) = [0.0 1.0]*x# state observation
+g(x) = C*x# state observation
 
 cstr_pf = PF.Model(f,g)
 
@@ -67,57 +67,65 @@ cstr_pf = PF.Model(f,g)
 nP = 100 #number of particles.
 init_state_mean = init_state # initial state mean
 init_state_covar = eye(2)*1e-6 # initial covariance
-init_state_covar = 1.0
+init_state_covar[4] = 2.0
 init_dist = MvNormal(init_state_mean, init_state_covar) # prior distribution
 particles = PF.init_PF(init_dist, nP, 2) # initialise the particles
-state_covar = eye(2) # state covariance
-state_covar[1] = 1e-6
-state_covar[2] = 2.
-state_dist = MvNormal(state_covar) # state distribution
-meas_covar = eye(1)*2.0 # measurement covariance
+state_dist = MvNormal(Q) # state distribution
+meas_covar = eye(1)*R # measurement covariance
 meas_dist = MvNormal(meas_covar) # measurement distribution
 
 fmeans = zeros(2, N)
 fcovars = zeros(2,2, N)
 # Time step 1
 xs[:,1] = init_state
-ys[1] = [0.0 1.0]*xs[:, 1] + rand(meas_dist) # measured from actual plant
+ys[1] = C*xs[:, 1] + rand(meas_dist) # measured from actual plant
 PF.init_filter!(particles, 0.0, ys[1], state_dist, meas_dist, cstr_pf)
 fmeans[:,1], fcovars[:,:,1] = PF.getStats(particles)
+filtermeans = zeros(2, N)
+filtercovars = zeros(2,2, N)
+filtermeans[:, 1], filtercovars[:,:, 1] = LLDS_functions.init_filter(init_state_mean, init_state_covar, ys[1], lin_cstr)
 # Loop through the rest of time
 for t=2:N
   xs[:, t] = Reactor_functions.run_reactor(xs[:, t-1], 0.0, h, cstr_model) # actual plant
-  ys[t] = [0.0 1.0]*xs[:, t] + rand(meas_dist) # measured from actual plant
+  ys[t] = C*xs[:, t] + rand(meas_dist) # measured from actual plant
   PF.filter!(particles, 0.0, ys[t], state_dist, meas_dist, cstr_pf)
   fmeans[:,t], fcovars[:,:,t] = PF.getStats(particles)
+  filtermeans[:, t], filtercovars[:,:, t] = LLDS_functions.step_filter(filtermeans[:, t-1], filtercovars[:,:, t-1], 0.0, ys[t], lin_cstr)
 end
 
 skip = 50
-figure(1) # Kalman Filter Demonstration
+figure(1) #  Filter Demonstration
 x1, = plot(xs[1,:][:], xs[2,:][:], "k", linewidth=3)
 f1, = plot(fmeans[1, 1:skip:end][:], fmeans[2, 1:skip:end][:], "rx", markersize=5, markeredgewidth = 2)
+f2, = plot(filtermeans[1, 1:skip:end][:], filtermeans[2, 1:skip:end][:], "mx", markersize=5, markeredgewidth = 2)
 b1 = 0.0
+b2 = 0.0
 for k=1:skip:N
   p1, p2 = Confidence.plot95(fmeans[:,k], fcovars[:,:, k])
   b1, = plot(p1, p2, "b")
+
+  p3, p4 = Confidence.plot95(filtermeans[:,k], filtercovars[:,:, k])
+  b2, = plot(p3, p4, "g")
 end
 ylabel("Temperature [K]")
 xlabel(L"Concentration [kmol.m$^{-3}$]")
-legend([x1,f1, b1],["Nonlinear Model","Particle Filter Mean", L"Particle Filter $1\sigma$-Ellipse"], loc="best")
+legend([x1,f1,f2, b1, b2],["Nonlinear Model","Particle Filter Mean","Kalman Filter Mean", L"Particle Filter $1\sigma$-Ellipse", L"Kalman Filter $1\sigma$-Ellipse"], loc="best")
 
 figure(2) # Plot filtered results
 subplot(2,1,1)
 x1, = plot(ts, xs[1,:]', "k", linewidth=3)
-k1, = plot(ts, fmeans[1,:]', "r--", linewidth=3)
+k1, = plot(ts[1:skip:end], fmeans[1,1:skip:end]', "r--", linewidth=3)
+k12, = plot(ts[1:skip:end], filtermeans[1, 1:skip:end]', "mo")
 ylabel(L"Concentration [kmol.m$^{-3}$]")
-legend([x1, k1],["Nonlinear Model","Filtered Mean"], loc="best")
+legend([x1, k1],["Nonlinear Model","Particle Filter"], loc="best")
 xlim([0, tend])
 subplot(2,1,2)
 x2, = plot(ts, xs[2,:]', "k", linewidth=3)
 y2, = plot(ts[1:10:end], ys[1:10:end], "kx", markersize=5, markeredgewidth=1)
-k2, = plot(ts, fmeans[2,:]', "r--", linewidth=3)
+k2, = plot(ts[1:skip:end], fmeans[2,1:skip:end]', "r--", linewidth=3)
+k22, = plot(ts[1:skip:end], filtermeans[2, 1:skip:end]', "mo")
 ylabel("Temperature [K]")
 xlabel("Time [min]")
-legend([y2],["Nonlinear Model Measured"], loc="best")
+legend([y2, k22],["Nonlinear Model Measured", "Kalman Filter"], loc="best")
 xlim([0, tend])
 rc("font",size=22)
