c1c9ef19a1af0cd8201ee34ca9cd9901a413d6f5
St Elmo <stelmozors@gmail.com>
2015-04-07 11:18:49 +0200

    modified test

--- test_all.jl
+++ test_all.jl
@@ -1,6 +1,19 @@
 # Testing script
 
-include("init_sys.jl")
+using Distributions
+
+# Bring into scope all the required packages and functions.
+for func in ["Ellipse.jl",
+            "HMM.jl",
+            "Burglar.jl",
+            "Reactor.jl",
+            "LLDS.jl",
+            "PF.jl",
+            "SPF.jl",
+            "RBPF.jl"]
+  include(joinpath("src",func))
+end
+
 include("./test/HMM_test.jl")
 include("./test/LLDS_test.jl")
 include("./test/PF_test.jl")
