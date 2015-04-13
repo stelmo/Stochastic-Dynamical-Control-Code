7502c8a2336a9a1d0b279317d5e84afe751a3c89
St Elmo <stelmozors@gmail.com>
2015-04-13 12:30:40 +0200

    RBPF state space fix and plots

--- openloop/Switching_Linear_Reactor_M1.jl
+++ openloop/Switching_Linear_Reactor_M1.jl
@@ -56,6 +56,7 @@ lin_cstr = LLDS.llds(A, B, C, Q, R)
 
 
 linsystems = Reactor.getLinearSystems(nX, nY, xspace, yspace, h, cstr_model)
+# linsystems = Reactor.getLinearSystems_randomly(0, xspace, yspace, h, cstr_model)
 models, A = RBPF.setup_RBPF(linsystems, C, Q, R)
 
 nP = 500
@@ -71,110 +72,136 @@ fcovars = zeros(2,2, N)
 filtermeans = zeros(2, N)
 filtercovars = zeros(2,2, N)
 switchtrack = zeros(length(linsystems), N)
+maxtrack = zeros(length(linsystems), N)
 
 state_dist = MvNormal(Q)
 us = zeros(N)
 measurements = MvNormal(R)
 xs[:,1] = initial_states
 ys[1] = C*xs[:, 1] + rand(measurements) # measured from actual plant
+
 RBPF.init_filter!(particles, 0.0, ys[1], models)
-fmeans[:,1], fcovars[:,:, 1] = RBPF.getStats(particles, models)
+# fmeans[:,1], fcovars[:,:, 1] = RBPF.getAveStats(particles)
+fmeans[:,1], fcovars[:,:, 1] = RBPF.getMLStats(particles)
+
 filtermeans[:, 1], filtercovars[:,:, 1] = LLDS.init_filter(initial_states-b, initial_covar, ys[1]-b[2], lin_cstr)
+
 for k=1:length(linsystems)
   switchtrack[k, 1] = sum(particles.ws[find((x)->x==k, particles.ss)])
 end
+maxtrack[:, 1] = RBPF.getMaxTrack(particles, models)
 
-##Loop through the rest of time
-for t=2:2
+#Loop through the rest of time
+for t=2:N
   xs[:, t] = Reactor.run_reactor(xs[:, t-1], us[t-1], h, cstr_model) + rand(state_dist) # actual plant
   ys[t] = C*xs[:, t] + rand(measurements) # measured from actual plant
 
   RBPF.filter!(particles, us[t-1], ys[t], models, A)
-  fmeans[:, t], fcovars[:,:, t] = RBPF.getStats(particles, models)
-  #
+  # fmeans[:, t], fcovars[:,:, t] = RBPF.getAveStats(particles)
+  fmeans[:,t], fcovars[:,:, t] = RBPF.getMLStats(particles)
+
   filtermeans[:, t], filtercovars[:,:, t] = LLDS.step_filter(filtermeans[:, t-1], filtercovars[:,:, t-1], us[t-1], ys[t]-b[2], lin_cstr)
+
   for k=1:length(linsystems)
     switchtrack[k, t] = sum(particles.ws[find((x)->x==k, particles.ss)])
   end
+  maxtrack[:, t] = RBPF.getMaxTrack(particles, models)
+
 end
 
 filtermeans = filtermeans .+ b
 
-#
-# rc("font", family="serif", size=24)
-#
-# figure(1)
-# for k=1:length(linsystems)
-#   plot(linsystems[k].op[1],linsystems[k].op[2],"kx",markersize=5, markeredgewidth=1)
-# annotate(string("Switch: ", k),
-#       xy=[linsystems[k].op[1],linsystems[k].op[2]],
-#       xytext=[linsystems[k].op[1],linsystems[k].op[2]],
-#       fontsize=22.0,
-#       ha="center",
-#       va="bottom")
-# end
-# plot(xs[1,:][:], xs[2,:][:], "k", linewidth=3)
-# plot(xs[1,1], xs[2,1], "ko", markersize=10, markeredgewidth = 4)
-# plot(xs[1,end], xs[2,end], "kx", markersize=10, markeredgewidth = 4)
-# xlim([-0.1, 1.1])
-# ylim([250, 650])
-# xlabel(L"Concentration [kmol.m$^{-3}$]")
-# ylabel("Temperature [K]")
-#
-# figure(2)
-# maxswitch = maximum(switchtrack)
-# axes = Array(Any, length(linsystems))
-# im = 0
-# width = 500
-# for k=1:length(linsystems)
-#   ax = subplot(length(linsystems), 1, k)
-#   axes[k] = ax
-#   im = imshow(repeat(switchtrack[k,:], outer=[width, 1]), cmap="cubehelix",vmin=0.0, vmax=maxswitch, interpolation="nearest", aspect="auto")
-#   tick_params(axis="y", which="both",left="off",right="off", labelleft = "off")
-#   tick_params(axis="x", which="both",bottom="off", labelbottom = "off")
-#   ylabel(string("S::",k))
-# end
-# tick_params(axis="x", labelbottom = "on")
-# xticks([1:int(length(ts)/10.0):length(ts)], ts[1:int(length(ts)/10.0):end])
-# colorbar(im, ax=axes)
-# xlabel("Time [min]")
-#
-#
-# figure(3) # Plot filtered results
-# skip = int(length(ts)/75)
-# subplot(2,1,1)
-# x1, = plot(ts, xs[1,:]', "k", linewidth=3)
-# k1, = plot(ts, fmeans[1,:]', "r--", linewidth=3)
-# ylabel(L"Concentration [kmol.m$^{-3}$]")
-# legend([x1, k1],["Nonlinear Model","Filtered Mean"], loc="best")
-# xlim([0, tend])
-# subplot(2,1,2)
-# x2, = plot(ts, xs[2,:]', "k", linewidth=3)
-# y2, = plot(ts[1:skip:end], ys[1:skip:end], "kx", markersize=5, markeredgewidth=1)
-# k2, = plot(ts, fmeans[2,:]', "r--", linewidth=3)
-# ylabel("Temperature [K]")
-# xlabel("Time [min]")
-# legend([y2],["Nonlinear Model Measured"], loc="best")
-# xlim([0, tend])
-#
-# skip = int(length(ts)/20)
-# figure(4) # Kalman Filter Demonstration
-# x1, = plot(xs[1,:][:], xs[2,:][:], "k",linewidth=3)
-# x11, = plot(xs[1, 1:skip:end][:], xs[2, 1:skip:end][:], "kx", markersize=5, markeredgewidth = 2)
-# f1, = plot(fmeans[1, 1:skip:end][:], fmeans[2, 1:skip:end][:], "rx", markersize=5, markeredgewidth = 2)
-# f2, = plot(filtermeans[1, 1:skip:end][:], filtermeans[2, 1:skip:end][:], "gx", markersize=5, markeredgewidth = 2)
-# b1 = 0.0
-# b2 = 0.0
-# for k=1:skip:N
-#   p1, p2 = Ellipse.ellipse(fmeans[:,k], fcovars[:,:, k])
-#   b1, = plot(p1, p2, "b")
-#
-#   p3, p4 = Ellipse.ellipse(filtermeans[:,k], filtercovars[:,:, k])
-#   b2, = plot(p3, p4, "g")
-# end
-# plot(xs[1,:][:], xs[2,:][:], "k", linewidth=3)
-# plot(xs[1,1], xs[2,1], "ko", markersize=10, markeredgewidth = 4)
-# plot(xs[1,end], xs[2,end], "kx", markersize=10, markeredgewidth = 4)
-# ylabel("Temperature [K]")
-# xlabel(L"Concentration [kmol.m$^{-3}$]")
-# legend([x1,f1,f2, b1, b2],["Nonlinear Model","Switching Kalman Filter Mean","Kalman Filter Mean", L"Switching Kalman Filter $1\sigma$-Ellipse",L"Kalman Filter $1\sigma$-Ellipse"], loc="best")
+rc("font", family="serif", size=24)
+
+figure(1)
+for k=1:length(linsystems)
+  plot(linsystems[k].op[1],linsystems[k].op[2],"kx",markersize=5, markeredgewidth=1)
+annotate(string("Switch: ", k),
+      xy=[linsystems[k].op[1],linsystems[k].op[2]],
+      xytext=[linsystems[k].op[1],linsystems[k].op[2]],
+      fontsize=22.0,
+      ha="center",
+      va="bottom")
+end
+plot(xs[1,:][:], xs[2,:][:], "k", linewidth=3)
+plot(xs[1,1], xs[2,1], "ko", markersize=10, markeredgewidth = 4)
+plot(xs[1,end], xs[2,end], "kx", markersize=10, markeredgewidth = 4)
+xlim([-0.1, 1.1])
+ylim([250, 650])
+xlabel(L"Concentration [kmol.m$^{-3}$]")
+ylabel("Temperature [K]")
+
+figure(2)
+maxswitch = maximum(switchtrack)
+axes = Array(Any, length(linsystems))
+im = 0
+width = 500
+for k=1:length(linsystems)
+  ax = subplot(length(linsystems), 1, k)
+  axes[k] = ax
+  im = imshow(repeat(switchtrack[k,:], outer=[width, 1]), cmap="cubehelix",vmin=0.0, vmax=maxswitch, interpolation="nearest", aspect="auto")
+  tick_params(axis="y", which="both",left="off",right="off", labelleft = "off")
+  tick_params(axis="x", which="both",bottom="off", labelbottom = "off")
+  ylabel(string("S::",k))
+end
+tick_params(axis="x", labelbottom = "on")
+xticks([1:int(length(ts)/10.0):length(ts)], ts[1:int(length(ts)/10.0):end])
+colorbar(im, ax=axes)
+xlabel("Time [min]")
+
+figure(3)
+axes = Array(Any, length(linsystems))
+im = 0
+width = 500
+for k=1:length(linsystems)
+  ax = subplot(length(linsystems), 1, k)
+  axes[k] = ax
+  im = imshow(repeat(maxtrack[k,:], outer=[width, 1]), cmap="cubehelix",vmin=0.0, vmax=1.0, interpolation="nearest", aspect="auto")
+  tick_params(axis="y", which="both",left="off",right="off", labelleft = "off")
+  tick_params(axis="x", which="both",bottom="off", labelbottom = "off")
+  ylabel(string("S::",k))
+end
+tick_params(axis="x", labelbottom = "on")
+xticks([1:int(length(ts)/10.0):length(ts)], ts[1:int(length(ts)/10.0):end])
+colorbar(im, ax=axes)
+xlabel("Time [min]")
+
+
+figure(4) # Plot filtered results
+skip = int(length(ts)/75)
+subplot(2,1,1)
+x1, = plot(ts, xs[1,:]', "k", linewidth=3)
+k1, = plot(ts, fmeans[1,:]', "r--", linewidth=3)
+ylabel(L"Concentration [kmol.m$^{-3}$]")
+legend([x1, k1],["Nonlinear Model","Filtered Mean"], loc="best")
+xlim([0, tend])
+subplot(2,1,2)
+x2, = plot(ts, xs[2,:]', "k", linewidth=3)
+y2, = plot(ts[1:skip:end], ys[1:skip:end], "kx", markersize=5, markeredgewidth=1)
+k2, = plot(ts, fmeans[2,:]', "r--", linewidth=3)
+ylabel("Temperature [K]")
+xlabel("Time [min]")
+legend([y2],["Nonlinear Model Measured"], loc="best")
+xlim([0, tend])
+
+skip = int(length(ts)/20)
+figure(5) # Kalman Filter Demonstration
+x1, = plot(xs[1,:][:], xs[2,:][:], "k",linewidth=3)
+x11, = plot(xs[1, 1:skip:end][:], xs[2, 1:skip:end][:], "kx", markersize=5, markeredgewidth = 2)
+f1, = plot(fmeans[1, 1:skip:end][:], fmeans[2, 1:skip:end][:], "rx", markersize=5, markeredgewidth = 2)
+f2, = plot(filtermeans[1, 1:skip:end][:], filtermeans[2, 1:skip:end][:], "bx", markersize=5, markeredgewidth = 2)
+b1 = 0.0
+b2 = 0.0
+for k=1:skip:N
+  p1, p2 = Ellipse.ellipse(fmeans[:,k], fcovars[:,:, k])
+  b1, = plot(p1, p2, "r")
+
+  p3, p4 = Ellipse.ellipse(filtermeans[:,k], filtercovars[:,:, k])
+  b2, = plot(p3, p4, "b")
+end
+plot(xs[1,:][:], xs[2,:][:], "k", linewidth=3)
+plot(xs[1,1], xs[2,1], "ko", markersize=10, markeredgewidth = 4)
+plot(xs[1,end], xs[2,end], "kx", markersize=10, markeredgewidth = 4)
+ylabel("Temperature [K]")
+xlabel(L"Concentration [kmol.m$^{-3}$]")
+legend([x1,f1,f2, b1, b2],["Nonlinear Model","Switching Kalman Filter Mean","Kalman Filter Mean", L"Switching Kalman Filter $1\sigma$-Ellipse",L"Kalman Filter $1\sigma$-Ellipse"], loc="best")
