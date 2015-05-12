88e838a9830af352c606fa4d43bac671b9601ad9
St Elmo <stelmozors@gmail.com>
2015-05-12 19:33:36 +0200

    pf linear mpc

---
+++ closedloop/MPC_PF_VAR.jl
@@ -0,0 +1,61 @@
+# Controller using the linear reactor model measuring both concentration and temperature.
+
+include("../params.jl") # load all the parameters and modules
+
+# Get the linear model
+linsystems = Reactor.getNominalLinearSystems(h, cstr_model) # cstr_model comes from params.jl
+opoint = 2 # the specific operating point we are going to use for control
+
+init_state = [0.5, 450] # random initial point near operating point
+
+# Set the state space model
+A = linsystems[opoint].A
+B = linsystems[opoint].B
+b = linsystems[opoint].b # offset from the origin
+
+# Set point
+# ysp = linsystems[1].op[1] - b[1] # Low concentration
+ysp = linsystems[2].op[1] - b[1] # Medium concentration
+# ysp = 0.55 - b[1]
+
+f(x, u, w) = Reactor.run_reactor(x, u, h, cstr_model) + w
+g(x) = C2*x # state observation
+
+cstr_pf = PF.Model(f,g)
+
+# Initialise the PF
+nP = 100 # number of particles.
+prior_dist = MvNormal(init_state, init_state_covar) # prior distribution
+particles = PF.init_PF(prior_dist, nP, 2) # initialise the particles
+state_noise_dist = MvNormal(Q) # state distribution
+meas_noise_dist = MvNormal(R2) # measurement distribution
+
+# First time step of the simulation
+xs[:,1] = init_state # set simulation starting point to the random initial state
+ys2[:, 1] = C2*xs[:, 1] + rand(meas_noise_dist) # measure from actual plant
+PF.init_filter!(particles, 0.0, ys2[:, 1], meas_noise_dist, cstr_pf)
+pfmeans[:,1], pfcovars[:,:,1] = PF.getStats(particles)
+
+# Setup MPC
+horizon = 150
+# add state constraints
+aline = 10. # slope of constraint line ax + by + c = 0
+cline = -403.0 # negative of the y axis intercept
+bline = 1.0
+
+us[1] = MPC.mpc_var(pfmeans[:, 1], pfcovars[:,:, 1], horizon, A, B, b, aline, bline, cline, QQ, RR, ysp, 25000.0, 1000.0, false, 1.0)# get the controller input
+tic()
+for t=2:N
+  xs[:, t] = Reactor.run_reactor(xs[:, t-1], us[t-1], h, cstr_model) + rand(state_noise_dist) # actual plant
+  ys2[:, t] = C2*xs[:, t] + rand(meas_noise_dist) # measure from actual plant
+  PF.filter!(particles, us[t-1], ys2[:, t], state_noise_dist, meas_noise_dist, cstr_pf)
+  pfmeans[:,t], pfcovars[:,:,t] = PF.getStats(particles)
+
+  us[t] = MPC.mpc_var(pfmeans[:, t]-b, pfcovars[:, :, t], horizon, A, B, b, aline, bline, cline, QQ, RR, ysp, 25000.0, 1000.0, false, 1.0)
+end
+toc()
+
+# # Plot the results
+Results.plotTracking(ts, xs, ys2, pfmeans, us, 2, ysp+b[1])
+
+Results.plotEllipses(ts, xs, pfmeans, pfcovars, "MPC", [aline, cline], linsystems[2].op, true)
